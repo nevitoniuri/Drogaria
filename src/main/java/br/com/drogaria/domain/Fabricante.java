8393a1af83f95a31007020c4b3924e19fed0154c
Iuri Neviton <iurineviton@gmail.com>
2021-08-27 10:49:53 -0300

    adiciona classes java

---
+++ src/main/java/br/com/drogaria/domain/Fabricante.java
@@ -0,0 +1,26 @@
+package br.com.drogaria.domain;
+
+public class Fabricante {
+	private int codigo;
+	private String descricao;
+
+	public Fabricante() {
+
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
---
+++ src/main/java/br/com/drogaria/domain/Produto.java
@@ -0,0 +1,50 @@
+package br.com.drogaria.domain;
+
+public class Produto {
+	private int codigo;
+	private String descricao;
+	private int quantidade;
+	private double preco;
+	private Fabricante fabricante;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+
+}
